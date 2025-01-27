777eda6ed2b4cb824d3162a4ec0dd98671847d35
Erica <ecgreene87@gmail.com>
2025-01-26 22:45:40 -0500

    add api

--- main.py
+++ main.py
@@ -26,21 +26,20 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+path = os.path.join(project_path, "model", "encoder.pkl") #path for the saved encoder
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = os.path.join(project_path, "model", "model.pkl")  #path for the saved model
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI() # your code here
 
 # TODO: create a GET on the root giving a welcome message
-@app.get("/")
+@app.get("/welcome")
 async def get_root():
     """ Say hello!"""
-    # your code here
-    pass
+    return {"welcome": "to the model API"}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -65,10 +64,9 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        data=data,
+        training=False,
+        cat_features=cat_features
     )
-    _inference = None # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed) #predict the result using data_processed
     return {"result": apply_label(_inference)}
